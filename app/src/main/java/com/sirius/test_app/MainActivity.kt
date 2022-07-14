d4edcf3637269f28c1d26ccf3d336cf3331eb915
kekulta <ZXSpectrum11>
2022-07-14 12:23:36 +0300

    Верстка старницы v.2

--- app/src/main/java/com/sirius/test_app/MainActivity.kt
+++ app/src/main/java/com/sirius/test_app/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.sirius.test_app
 
-import androidx.appcompat.app.AppCompatActivity
+
+import android.content.Context
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val ab: ActionBar? = supportActionBar
+        val inflator = this.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view: View = inflator.inflate(R.layout.actionbar, null)
+        with(ab) {
+            this?.customView = view
+            this?.setDisplayShowCustomEnabled(true)
+        }
+
     }
 }
